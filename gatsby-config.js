bafe2fc01f51167cd3fc59db0061e59281eb89fa
Mayvis <kevin73911@gmail.com>
2021-10-04 22:38:54 +0800

    Update: favicon config background color

--- gatsby-config.js
+++ gatsby-config.js
@@ -116,7 +116,7 @@ module.exports = {
         short_name: `GatsbyJS`,
         start_url: `/`,
         background_color: `#ffffff`,
-        theme_color: `#663399`,
+        theme_color: `#037f32`,
         display: `minimal-ui`,
         icon: `src/assets/mayvis-icon.png`, // This path is relative to the root of the site.
       },
